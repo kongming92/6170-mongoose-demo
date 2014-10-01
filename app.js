7d8bfb3230e052a84056eebd471a36f2aa76b4ad
Charles Liu <cliu2014@mit.edu>
2014-10-01 02:26:32 -0400

    Remove non-mongoose data files

--- app.js
+++ app.js
@@ -14,11 +14,7 @@ app.get('/', function (req, res) {
 });
 
 // uncomment one of the following requires to select a version of the app
-// movies-embed: plain Mongo API, with theater records embedded in movie records
-// movies-join: plain Mongo API, relational-style schema, with theater records joined to movie records by ids
 // movies-mongoose: Mongoose on top of Mongo, with relational-style schema
-//var movieRoutes = require('./routes/movies-embed');
-//var movieRoutes = require('./routes/movies-join');
 var movieRoutes = require('./routes/movies-mongoose');
 app.use('/movies', movieRoutes);
 
